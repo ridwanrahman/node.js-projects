a1c0c66619fe9c5f4961a7006e0358ab1f92d97a
ridwanrahman <ridwan_rahman@ymail.com>
2021-01-22 23:27:34 +1100

    added node-persist

---
+++ getting input and storing data/app.js
@@ -0,0 +1,13 @@
+console.log("starting password manager");
+
+const storage = require('node-persist');
+storage.initSync();
+
+function createAccount(account) {
+
+}
+
+function getAccount(username) {
+    
+}
+
